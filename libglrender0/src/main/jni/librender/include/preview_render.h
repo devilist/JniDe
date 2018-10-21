fdad3ac80f4522f6127a16ba33dfc68ec31d3f19
devilist <bit_zengpu@163.com>
2018-10-21 21:41:34 +0800

    封装render

---
+++ libglrender0/src/main/jni/librender/include/preview_render.h
@@ -0,0 +1,80 @@
+//
+// Created by zengp on 2018/10/21.
+//
+
+
+#ifndef JNIDE_PREVIEW_RENDER_H
+#define JNIDE_PREVIEW_RENDER_H
+
+#include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
+#include "preview_texture.h"
+
+static const char *PREVIEW_VERTEX_SHADER =
+        "attribute vec4 position;       \n"
+                "attribute vec2 texcoord;       \n"
+                "varying vec2 v_texcoord;       \n"
+                "void main(void)                \n"
+                "{                              \n"
+                "   gl_Position = position;     \n"
+                "   v_texcoord = texcoord;      \n"
+                "}                              \n";
+
+static const char *PREVIEW_FRAG_SHADER =
+        "varying highp vec2 v_texcoord;                         \n"
+                "uniform sampler2D yuvTexSampler;                       \n"
+                "void main() {                                          \n"
+                "  gl_FragColor = texture2D(yuvTexSampler, v_texcoord); \n"
+                "}\n";
+
+//物体顶点坐标
+static const GLfloat vertices[] = {-1f, 1f,
+                                   -1f, -1f,
+                                   1f, 1f,
+                                   1f, -1f};
+
+// 纹理坐标
+static const GLfloat texCoords[] = {0f, 0f,
+                                    0f, 1f,
+                                    1f, 0f,
+                                    1f, 1f};
+
+class PreviewRender
+{
+protected:
+    GLint backingLeft;
+    GLint backingTop;
+    GLint backingWidth;
+    GLint backingHeight;
+
+    GLuint vertShader;
+    GLuint fragShader;
+
+    PreviewTexture *texture;
+    GLint uniformSampler;
+    GLuint program;
+
+public:
+    PreviewRender();
+
+    virtual ~PreviewRender();
+
+    virtual bool init(int width, int height, PreviewTexture *texture);
+
+    virtual void render();
+
+    virtual void dealloc();
+
+    void resetRenderSize(int left, int top, int width, int height);
+
+protected:
+    int useProgram();
+
+    int initShaders();
+
+    GLuint compileShader(GLenum type, const char *shaderSource);
+
+    bool checkGlError(const char *where);
+};
+
+#endif //JNIDE_PREVIEW_RENDER_H
---
+++ libglrender0/src/main/jni/librender/preview_render.cpp
@@ -0,0 +1,164 @@
+//
+// Created by zengp on 2018/10/21.
+//
+
+#include "include/preview_render.h"
+#include "../libutils/my_log.h"
+
+#define LOG_TAG "PreviewRender"
+
+enum
+{
+    ATTRIBUTE_VERTEX, ATTRIBUTE_TEXCOORD,
+};
+
+PreviewRender::PreviewRender()
+{
+
+}
+
+PreviewRender::~PreviewRender()
+{
+
+}
+
+bool PreviewRender::init(int width, int height, PreviewTexture *texture)
+{
+    MY_LOGE(LOG_TAG, "PreviewRender init");
+    this->backingLeft = 0;
+    this->backingTop = 0;
+    this->backingWidth = width;
+    this->backingHeight = height;
+    this->texture = texture;
+
+    vertShader = 0;
+    fragShader = 0;
+    program = 0;
+
+    int res = initShaders();
+    if (res < 0) {
+        MY_LOGI(LOG_TAG, "PreviewRender init shader failed ");
+        return false;
+    }
+
+    res = this->useProgram();
+    if (res < 0) {
+        MY_LOGI(LOG_TAG, "PreviewRender use program failed ");
+        this->dealloc();
+        return false;
+    }
+    return false;
+}
+
+void PreviewRender::render()
+{
+    MY_LOGE(LOG_TAG, "PreviewRender render");
+    glViewport(backingLeft, backingTop, backingWidth, backingHeight);
+    glClearColor(0f, 0f, 0f, 0f);
+    glClear(GL_COLOR_BUFFER_BIT);
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
+    glUseProgram(program);
+    // 设置物体顶点坐标
+    glVertexAttribPointer(ATTRIBUTE_VERTEX, 2, GL_FLOAT, GL_FALSE, 0, vertices);
+    glEnableVertexAttribArray(ATTRIBUTE_VERTEX);
+    // 设置纹理坐标
+    glVertexAttribPointer(ATTRIBUTE_TEXCOORD, 2, GL_FLOAT, GL_FALSE, 0, texCoords);
+    glEnableVertexAttribArray(ATTRIBUTE_TEXCOORD);
+    // 激活纹理，将要绘制的纹理传给shader
+    texture->bindTexture(uniformSampler);
+    // 绘制
+    glDrawArrays(GL_TRIANGLE_STRIP,
+                 0,    // 顶点数组中的起始顶点索引
+                 4);   // 绘制的顶点数量
+}
+
+void PreviewRender::dealloc()
+{
+    if (vertShader) {
+        glDeleteShader(vertShader);
+    }
+    if (fragShader) {
+        glDeleteShader(fragShader);
+    }
+    if (texture) {
+        texture->dealloc();
+    }
+    if (program) {
+        glDeleteProgram(program);
+        program = 0;
+    }
+}
+
+void PreviewRender::resetRenderSize(int left, int top, int width, int height)
+{
+    this->backingLeft = left;
+    this->backingTop = top;
+    this->backingWidth = width;
+    this->backingHeight = height;
+}
+
+int PreviewRender::useProgram()
+{
+    program = glCreateProgram();
+    glAttachShader(program, vertShader);
+    glAttachShader(program, vertShader);
+    glBindAttribLocation(program, ATTRIBUTE_VERTEX, "position");
+    glBindAttribLocation(program, ATTRIBUTE_TEXCOORD, "texcoord");
+    glLinkProgram(program);
+    GLint status;
+    glGetProgramiv(program, GL_LINK_STATUS, &status);
+    if (status == GL_FALSE) {
+        MY_LOGI(LOG_TAG, "Failed to link program %d", program);
+        return -1;
+    }
+    glUseProgram(program);
+    uniformSampler = glGetUniformLocation(program, "yuvTexSampler");
+
+    return 1;
+}
+
+int PreviewRender::initShaders()
+{
+    vertShader = compileShader(GL_VERTEX_SHADER, PREVIEW_VERTEX_SHADER);
+    if (!vertShader) {
+        return -1;
+    }
+    fragShader = compileShader(GL_FRAGMENT_SHADER, PREVIEW_FRAG_SHADER);
+    if (!fragShader) {
+        return -1;
+    }
+    return 1;
+}
+
+GLuint PreviewRender::compileShader(GLenum type, const char *shaderSource)
+{
+    GLint status;
+    GLuint shader = glCreateShader(type);
+    if (shader == 0 || shader == GL_INVALID_ENUM) {
+        MY_LOGI(LOG_TAG, "Failed to create shader %d", type);
+        return 0;
+    }
+    glShaderSource(shader, 1, &shaderSource, NULL);
+    glCompileShader(shader);
+
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+    if (status == GL_FALSE) {
+        glDeleteShader(shader);
+        MY_LOGI(LOG_TAG, "Failed to compile shader : %s\n", shaderSource);
+        return 0;
+    }
+    return shader;
+}
+
+bool PreviewRender::checkGlError(const char *where)
+{
+    GLint error;
+    for (error = glGetError(); error; error = glGetError()) {
+        MY_LOGE(LOG_TAG, "error: after %s() glError (0x%x)\n", where, error);
+        return true;
+    }
+    return false;
+}
+
